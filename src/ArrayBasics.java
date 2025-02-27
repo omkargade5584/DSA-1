3331a4c02b9629804a5b24d1ed08445a302e54ca
omkargade5584 <omkargade2804@gmail.com>
2025-02-27 21:47:50 +0530

    Added ArrayBasics program

---
+++ src/ArrayBasics.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ArrayBasics {
+    //declaration of Array
+    //int arr[]; //this array will store the int type element
+    //char arr[]; //this array will store the char type element
+    //float arr[]; //this array will store the float tupe element
+
+    //initilization of Array
+    //int arr[]={1,2,3,4,5};
+    //char arr[]={'a','b','c','d','e'};
+    //float arr[]={1.4f, 2.0f, 24f, 5.0f, 0.0f};
+
+    //fixed size array
+    //int [] arr1 = new int[5];
+    //int [] arr2 = {1,2,3,4,5};
+
+    //Dynamic size array
+    //ArrayList<Integer> arr = new ArrayList <>();
+
+    //System.out.println(arr.length);  //Print length of the array
+
+    for(int=0; i<arr.length; i++ ){
+        System.out.println(arr[i]);
+    }
+    for (int num : arr){
+        System.out.println(num);
+    }
+}
