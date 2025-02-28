c65d981a81586a092e8469a4722ac25ba55cc8d7
omkargade5584 <omkargade2804@gmail.com>
2025-02-28 21:51:56 +0530

    Added LinearSearch Basic program

---
+++ src/LinearSearch.java
@@ -0,0 +1,20 @@
+public class LinearSearch {
+    public static int search(arr[], int N, int X)
+    {
+        for(int i = 0; i < N; i++){
+            if(arr[i] ==X)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] ={ 2,3,4,10,40};
+        int x =10;
+
+        int result =search (arr, arr.length, x);
+        if(result == -1)
+            System.out.println("Element not found");
+        else
+            System.out.println("Element found" + result);
+    }
+}
