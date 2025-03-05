ecfabc59fd086716e83cb1c5746f845559057052
omkargade5584 <omkargade2804@gmail.com>
2025-03-05 16:47:35 +0530

    Added SelectionSort Basic program

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class SelectionSort {
+    static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) { // Outer loop for each position
+            int minIndex = i;  // Assume the first element of the unsorted part is the minimum
+
+            for (int j = i + 1; j < n; j++) {  // Inner loop to find the minimum element
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;  // Update minIndex if a smaller element is found
+                }
+            }
+
+            // Swap the found minimum element with the first element of the unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
