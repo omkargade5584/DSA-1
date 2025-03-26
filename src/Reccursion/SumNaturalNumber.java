bbed3b007151d4e10868990ee71f0a1ccd5644c7
omkargade5584 <omkargade2804@gmail.com>
2025-03-26 15:10:40 +0530

    Refactor the packages and code

---
+++ src/Reccursion/SumNaturalNumber.java
@@ -0,0 +1,12 @@
+package Reccursion;
+
+public class SumNaturalNumbers {
+    public static int sum(int n) {
+        if (n == 1) return 1; // Base case
+        return n + sum(n - 1); // Recursive call
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5)); // Output: 15 (1+2+3+4+5)
+    }
+}
